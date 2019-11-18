0d2cb2216141abed69d377eae6b5f9313137bf6d
jagreehal <jag.reehal@gmail.com>
2019-11-18 21:33:28 +0000

    better handling of routes

--- packages/nextjs-app/server.js
+++ packages/nextjs-app/server.js
@@ -1,28 +1,24 @@
-const stencil = require('stencil-web-components/hydrate');
-const express = require('express');
-const next = require('next');
+const stencil = require("stencil-web-components/hydrate");
+const express = require("express");
+const next = require("next");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/a', async (req, res) => {
-    const html = await app.renderToHTML(req, res, '/a', req.query);
-    const renderedHtml = await stencil.renderToString(html);
-    res.send(renderedHtml.html);
-  });
-  server.get('/b', async (req, res) => {
-    const html = await app.renderToHTML(req, res, '/b', req.query);
-    const renderedHtml = await stencil.renderToString(html);
-    res.send(renderedHtml.html);
+  // static assets
+  server.get("/_next/*", (req, res) => {
+    return handle(req, res);
   });
 
-  server.all('*', (req, res) => {
-    return handle(req, res);
+  server.all("*", async (req, res) => {
+    const html = await app.renderToHTML(req, res, req.path, req.query);
+    const renderedHtml = await stencil.renderToString(html);
+    return res.send(renderedHtml.html);
   });
 
   server.listen(port, err => {
